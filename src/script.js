9465200251d9c0e6a2a1fe9d29debb3414009af1
Arya Vishwakarma <98632890+AryaViscedcoss1@users.noreply.github.com>
2022-02-10 10:05:00 +0530

    Initializing Varia

--- src/script.js
+++ src/script.js
@@ -1,656 +1,90 @@
-/* before and */
-let a=1;
-var b= 5;
-var myVar = 0;
-let s="string";
-var v;
-var V;
-console.log(v.id,V.id)
-let e=1; /*can't be changed*/
-var f=1;
-var f=0;
-var decimal=3.14;
-const k=1;
-console.log(1 +3);
-console.log(5-4);
-console.log(8*4);
-console.log(8/4);
-console.log(f++);
+$(document).ready(function()
+{ 
+  $('#update').hide();
+  $('.error').hide();
+  $('.success').hide();
+  $("#add_product").click(function()
+  { 
+    console.log('clicked on add product')
+    var sku=$("#product_sku").val();
+    var name=$("#product_name").val();
+    var price=$("#product_price").val();
+    var quantity=$("#product_quantity").val();
+
+    //error
+
+    if(sku=="" || name=="" || price=="" || quantity=="")
+    {    
+      $('.success').hide();
+         $('.error').show()
+         return ;
+    }
+
+
+    var txt1 = $("<td></td>").text(sku);
+    var txt2 = $("<td></td>").text(name); 
+    var txt3 = $("<td></td>").text(price); 
+    var txt4 = $("<td></td>").text(quantity); 
+    var edit = $("<button>edit</button>");
+    var deletes = $("<button>delete</button>");
+    var txt =$("<tr></tr>").append(txt1,txt2,txt3,txt4);
+
+
+    $(txt).append(edit);
+    $(txt).append(deletes);
+    $('table').append(txt);
+
+
+    // edit click
+    $(edit).click(function ()
+    {
+      console.log('clicked on edit');
+      $("#product_sku").val(sku);
+      $("#product_name").val(name);
+      $("#product_price").val(price);
+      $("#product_quantity").val(quantity);
+
+        $('#update').click(function()
+        {
+
+          sku=$("#product_sku").val();
+          name=$("#product_name").val();
+          price=$("#product_price").val();
+          quantity=$("#product_quantity").val();
+
+          console.log('clicked on update');
+
+         $(txt1).text(sku);
+         $(txt2).text(name);
+          $(txt3).text(price);
+          $(txt4).text(quantity);
+
+          $('#update').hide();
+          $('#add_product').show();
+
+        })
+
+
+        $('#add_product').hide()
+      $('#update').show();
+    });
+
+    //delete button
+    $(deletes).click(function ()
+    {
+      console.log('clicked on delete');
+      $(txt1).remove();
+      $(txt2).remove();
+      $(txt3).remove();
+      $(txt4).remove();
+      $(edit).remove();
+      $(deletes).remove();
+
+    });
+    $('.error').hide();
+    $('.success').show();
+  })
+
+  
+});
 
-console.log(b--);
-console.log(1.5 * 1.4);
-console.log(1.5 / 1.4);
-console.log(5%2);
-console.log(f+=5);
-console.log(f-=2);
-console.log(f*=2);
-console.log(f/=2);
-const myStr = "I am a \"double quoted\" string inside \"double quotes\"."; // Change this line
-const myStr = '<a href="http://www.example.com" target="_blank">Link</a>';//Quoting Strings with Single Quotes
-//Concatenating Strings with the Plus Equals  Operator
-let myStr="This is the first sentence.";
-myStr=" This is the second sentence.";
-//Concatenating Strings with Plus  Operator
-const myStr = "This is the start."+ " This is the end."; // Change this line
-// Only change code below this line
-const myName = "Arya";
-const myStr = "My name is "+ myName+ "and I am well!";
-// Change code below this line
-
-const someAdjective = "arya";
-let myStr = "Learning to code is ";
-myStr+=someAdjective;
-// Setup
-let lastNameLength = 0;
-const lastName = "Lovelace";
-
-// Only change code below this line
-lastNameLength = lastName.length;
-//Use Bracket Notation to Find the First Character in a String
-// Setup
-let firstLetterOfLastName = "";
-const lastName = "Lovelace";
-
-// Only change code below this line
-firstLetterOfLastName = lastName[0]; // Change this line
-// Setup
-let myStr = "Jello World";
-
-// Only change code below this line
-myStr= "Hello World"; // Change this line
-// Only change code above this line
-// Setup
-const lastName = "Lovelace";
-
-// Only change code below this line
-const thirdLetterOfLastName = lastName[2]; // Change this line
-
-// Setup
-const lastName = "Lovelace";
-
-// Only change code below this line
-const lastLetterOfLastName = lastName[lastName.length-1]; // Change this line
-// Setup
-const lastName = "Lovelace";
-
-// Only change code below this line
-const secondToLastLetterOfLastName = lastName[lastName.length - 2]; // Change this line
-
-const myNoun = "dog";
-const myAdjective = "big";
-const myVerb = "ran";
-const myAdverb = "quickly";
-
-// Only change code below this line
-const wordBlanks = myNoun+" "+myAdjective +" "+myVerb+" "+myAdverb; // Change this line
-// Only change code above this line
-// Only change code below this line
-const myArray = [" ",23];
-// Only change code below this line
-const myArray = [["Bulls", 23], ["White Sox", 45]];
-// Setup
-const myArray = [18, 64, 99];
-
-// Only change code below this line
-myArray[0]=45;
-const myArray = [
-  [1, 2, 3],
-  [4, 5, 6],
-  [7, 8, 9],
-  [[10, 11, 12], 13, 14],
-];
-
-const myData = myArray[2][1];
-// Setup
-const myArray = [["John", 23], ["cat", 2]];
-
-// Only change code below this line
-myArray.push(["dog", 3]);// Setup
-const myArray = [["John", 23], ["cat", 2]];
-
-// Only change code below this line
-const removedFromMyArray=myArray.pop();
-// Setup
-const myArray = [["John", 23], ["dog", 3]];
-
-// Only change code below this line
-var removedFromMyArray= myArray.shift();
-// Setup
-const myArray = [["John", 23], ["dog", 3]];
-myArray.shift();
-
-// Only change code below this line
-myArray.unshift(["Paul", 35])
-const myList = [["s",1],["d",2],["f",33],["h",4],["m",5]];
-function reusableFunction()
-{
-  console.log("Hi World");
-}
-
-reusableFunction();
-
-function functionWithArgs(a1,a2)
-{
-  console.log(a1+a2);
-}
-functionWithArgs(1,5)
-function timesFive(a)
-{
- return 5*a;
-}
-
-// Declare the myGlobal variable below this line
-
-
-function fun1() {
-  // Assign 5 to oopsGlobal Here
-      oopsGlobal=5
-}
-
-// Only change code above this line
-let myGlobal=10;
-function fun2() {
-  var output = "";
-  if (typeof myGlobal != "undefined") {
-    output += "myGlobal: " + myGlobal;
-  }
-  if (typeof oopsGlobal != "undefined") {
-    output += " oopsGlobal: " + oopsGlobal;
-  }
-  console.log(output);
-}
-
-function myLocalScope() {
-  // Only change code below this line
-   let myVar=5
-  console.log('inside myLocalScope', myVar);
-}
-myLocalScope();
-
-// Run and check the console
-// myVar is not defined outside of myLocalScope
-console.log('outside myLocalScope', myVar);
-// Setup
-const outerWear = "T-Shirt";
-
-function myOutfit() {
-  // Only change code below this line
-let outerWear = "sweater"
-  // Only change code above this line
-  return outerWear;
-}
-
-myOutfit();
-// Setup
-let sum = 0;
-
-function addThree() {
-  sum = sum + 3;
-}
-
-// Only change code below this line
-function addFive() {
-  sum = sum + 5;
-}
-
-// Only change code above this line
-
-addThree();
-addFive();
-
-// Setup
-let processed = 0;
-
-function processArg(num) {
-  return (num + 3) / 5;
-}
-
-// Only change code below this line
-
-processed=processArg(7);
-function trueOrFalse(wasThatTrue) {
-  // Only change code below this line
- if (wasThatTrue==true)
- {
-   return "Yes, that was true";
- }
- else
- {
-   return "No, that was false";
- }
-
-
-  // Only change code above this line
-
-}
-// Setup
-function testEqual(val) {
-  if (val==12) { // Change this line
-    return "Equal";
-  }
-  return "Not Equal";
-}
-
-testEqual(10);
-// Setup
-function testStrict(val) {
-  if (val===7) { // Change this line
-    return "Equal";
-  }
-  return "Not Equal";
-}
-
-testStrict(10);
-
-// Setup
-function compareEquality(a, b) {
-  if (a === b) { // Change this line
-    return "Equal";
-  }
-  return "Not Equal";
-}
-
-compareEquality(10, "10");
-// Setup
-function testStrictNotEqual(val) {
-  if (val!==17) { // Change this line
-    return "Not Equal";
-  }
-  return "Equal";
-}
-
-testStrictNotEqual(10);
-function testGreaterThan(val) {
-  if (val>100) {  // Change this line
-    return "Over 100";
-  }
-
-  if (val>10) {  // Change this line
-    return "Over 10";
-  }
-
-  return "10 or Under";
-}
-
-testGreaterThan(10);
-function testLessThan(val) {
-  if (val<25) {  // Change this line
-    return "Under 25";
-  }
-
-  if (val<55) {  // Change this line
-    return "Under 55";
-  }
-
-  return "55 or Over";
-}
-
-testLessThan(10);
-function testLessOrEqual(val) {
-  if (val<=12) {  // Change this line
-    return "Smaller Than or Equal to 12";
-  }
-
-  if (val<=24) {  // Change this line
-    return "Smaller Than or Equal to 24";
-  }
-
-  return "More Than 24";
-}
-
-testLessOrEqual(10);
-
-function testElse(val) {
-  let result = "";
-  // Only change code   below this line
-
-  if (val > 5) {
-    result = "Bigger than 5";
-  }
-
-  else{
-    result = " 5 or Smaller";
-  }
-
-   
-  // Only change   code  above  this   line
-  return result; 
-}
-
-testElse(4);
-
-
-
-//Multiple Identical Options in Switch Statements
-
-function sequentialSizes(val) {
-  let answer = "";
-  // Only change code below this line
-  switch(val)
-  {
-    case 1:
-    answer="Low";
-    break;
-    case 2:
-    answer="Low";
-    break;
-    case 3:
-    answer="Low";
-    break;
-
-    case 4:
-    answer="Mid";
-    break;
-    case 5:
-    answer="Mid";
-    break;
-    case 6:
-    answer="Mid";
-    break;
-
-    case 7:
-    answer="High"
-    break;
-    case 8:
-    answer="High"
-    break;
-    case 9:
-    answer="High"
-    break;
-  }
-
-  // Only change code above this line
-  return answer;
-}
-
-sequentialSizes(1);
-
-
-//Replacing If Else Chains with Switch
-
-function chainToSwitch(val) {
-  let answer = "";
-  // Only change code below this line
-  switch(val)
-  {
-  case "bob":
-    answer = "Marley";
-    break;
-  case 42 :
-    answer = "The Answer";
-    break;
-  case 1 :
-    answer = "There is no #1";
-    break;
-  case 99:
-    answer = "Missed me by this much!";
-    break;
-  case 7 :
-    answer = "Ate Nine";
-    break;
-  }
-
-  // Only change code above this line
-  return answer;
-}
-
-chainToSwitch(7);
-
-
-//Returning Boolean Values from Functions
-
-
-function isLess(a, b) {
-  // Only change code below this line
-    return a<b;
-  // Only change code above this line
-}
-
-isLess(10, 15);
-
-
-//Build JavaScript Objects
-
-const myDog = {
-  // Only change code below this line
-    name: "Ford",
-  legs: 4,
-  tails:1,
-  friends: []
-
-  // Only change code above th
-
-// Setup
-const testObj = {
-  "hat": "ballcap",
-  "shirt": "jersey",
-  "shoes": "cleats"
-};
- 
-// Only change code below this line
-const hatValue = testObj.hat;      // Chang e this line
-const shirtValue = testObj.shirt;    // Change this line
-
-//// Setup
-const myDog = {
-  "name": "Coder",
-  "legs": 4,
-  "tails": 1,
-  "friends": ["freeCodeCamp Campers"]
-};
-
-// Only change code below this line
-myDog.name="Happy Coder"
-
-
-// Setup
-/*
-const myDog = {
-  "name": "Coder",
-  "legs": 4,
-  "tails": 1,
-  "friends": ["freeCodeCamp Campers"]
-};
-
-// Only change code below this line
-myDog.name="Happy Coder";*/
-
-//Use the parseInt Function
-// Setup
-const testObj = {
-  "hat": "ballcap",
-  "shirt": "jersey",
-  "shoes": "cleats"
-};
-
-// Only change code below this line
-const hatValue = testObj.hat;      // Change this line
-const shirtValue = testObj.shirt;    // Change this line
-
-// Setup
-const myDog = {
-  "name": "Coder",
-  "legs": 4,
-  "tails": 1,
-  "friends": ["freeCodeCamp Campers"]
-};
-
-// Only change code below this line
-myDog.name="Happy Coder";
-// Setup
-const recordCollection = {
-  2548: {
-    albumTitle: 'Slippery When Wet',
-    artist: 'Bon Jovi',
-    tracks: ['Let It Rock', 'You Give Love a Bad Name']
-  },
-  2468: {
-    albumTitle: '1999',
-    artist: 'Prince',
-    tracks: ['1999', 'Little Red Corvette']
-  },
-  1245: {
-    artist: 'Robert Palmer',
-    tracks: []
-  },
-  5439: {
-    albumTitle: 'ABBA Gold'
-  }
-};
-
-// Only change code below this line
-function updateRecords(records, id, prop, value) {
-  return records;
-}
-
-updateRecords(recordCollection, 5439, 'artist', 'ABBA');
-
-const ourArray = [];
-
-for (let i = 0; i < 5; i++) {
-  ourArray.push(i);
-}
-
-const ourArray = [];
-
-for (let i = 10; i > 0; i -= 2) {
-  ourArray.push(i);
-}
-
-const arr = [10, 9, 8, 7, 6];
-
-for (let i = 0; i < arr.length; i++) {
-   console.log(arr[i]);
-}
-
-
-
-//Multiple Identical Options in Switch Statements
-
-function sequentialSizes(val) {
-  let answer = "";
-  // Only change code below this line
-  switch(val)
-  {
-    case 1:
-    answer="Low";
-    break;
-    case 2:
-    answer="Low";
-    break;
-    case 3:
-    answer="Low";
-    break;
-
-    case 4:
-    answer="Mid";
-    break;
-    case 5:
-    answer="Mid";
-    break;
-    case 6:
-    answer="Mid";
-    break;
-
-    case 7:
-    answer="High"
-    break;
-    case 8:
-    answer="High"
-    break;
-    case 9:
-    answer="High"
-    break;
-  }
-
-  // Only change code above this line
-  return answer;
-}
-
-sequentialSizes(1);
-
-
-//Replacing If Else Chains with Switch
-
-function chainToSwitch(val) {
-  let answer = "";
-  // Only change code below this line
-  switch(val)
-  {
-  case "bob":
-    answer = "Marley";
-    break;
-  case 42 :
-    answer = "The Answer";
-    break;
-  case 1 :
-    answer = "There is no #1";
-    break;
-  case 99:
-    answer = "Missed me by this much!";
-    break;
-  case 7 :
-    answer = "Ate Nine";
-    break;
-  }
-
-  // Only change code above this line
-  return answer;
-}
-
-chainToSwitch(7);
-
-
-//Returning Boolean Values from Functions
-
-
-function isLess(a, b) {
-  // Only change code below this line
-    return a<b;
-  // Only change code above this line
-}
-
-isLess(10, 15);
-
-
-//Build JavaScript Objects
-
-const myDog = {
-  // Only change code below this line
-    name: "Ford",
-  legs: 4,
-  tails:1,
-  friends: []
-
-  // Only change code above th
-
-// Setup
-const testObj = {
-  "hat": "ballcap",
-  "shirt": "jersey",
-  "shoes": "cleats"
-};
-
-
-
-
-function multiplyAll(arr) {
-  let product = 1;
-  // Only change code below this line
-
-  // Only change code above this line
-  return product;
-}
-
-multiplyAll([[1, 2], [3, 4], [5, 6, 7]]);
-
-
-// Setup
-const myArray = [];
-let i = 10;
-
-// Only change code below this line
-while (i < 5) {
-  myArray.push(i);
-  i++;
-}
